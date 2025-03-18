b52605ff239e85b1205325ea75a8620ea414ab94
BhaveshLaku <bhaveshlakupbl@gmail.com>
2025-03-19 00:57:43 +0530

    Initial commit

---
+++ src/main/java/com/bhavesh/airbnbapp/entity/Inventory.java
@@ -0,0 +1,4 @@
+package com.bhavesh.airbnbapp.entity;
+
+public class Inventory {
+}
---
+++ src/test/java/com/bhavesh/airbnbapp/AirBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh.airbnbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

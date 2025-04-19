bee66d8f00764858dd3177936d448d74dc66a8db
bhaveshlaku <bhaveshlakupbl@gmail.com>
2025-04-20 04:45:47 +0530

    add partial add guest functionality

--- src/main/java/com/bhavesh/airbnbapp/strategy/BasePricingStrategy.java
+++ src/main/java/com/bhavesh/airbnbapp/strategy/BasePricingStrategy.java
@@ -4,7 +4,7 @@ import com.bhavesh.airbnbapp.entity.Inventory;
 
 import java.math.BigDecimal;
 
-
+//
 public class BasePricingStrategy implements PricingStrategy {
     @Override
     public BigDecimal calculatePrice(Inventory inventory) {
